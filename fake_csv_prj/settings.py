383b155d98d78f9f36631ba80e022af3cf0674ed
Arsemon4ik <misterarsemon4ik@gmail.com>
2023-03-15 18:32:50 +0200

    remove hardcoded settings using .env

--- fake_csv_prj/settings.py
+++ fake_csv_prj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os.path
 from pathlib import Path
+from os import getenv
 import mimetypes
 
 # production settings
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SUPER_SECRET_KEY12345'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
